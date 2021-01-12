997cad34256968d96b7feb9540e05d5a831ccd31
Denis <rzyszka@tlen.pl>
2021-01-12 20:15:05 +0100

    Added filters

--- krawedzie.py
+++ krawedzie.py
@@ -3,9 +3,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 import project
 
-def krawedzie(source):
 
-    img = cv.imread(source,0)
-    edges = cv.Canny(img,100,200)
+def krawedzie(source):
+    img = cv.imread(source, 0)
+    edges = cv.Canny(img, 100, 200)
     edges = project.cv_to_pil(edges)
     return edges
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import project
 import OCR
 import krawedzie
+import filtry
 import cv2 as cv
 from PIL import Image
 import numpy
 
 # source = input("Enter path to image: ")
-source = "6.jpg"
+source = "7.jpg"
 
-scaled = project.scale(source, 2)
+# scaled = project.scale(source, 0.1)
 # grayed = project.gray(source)
 # luminesence = project.luminesence(source, 200)
 # negative = project.negative(source)
 # threshold = project.threshold(source, 50, 135)
 # opticalCharacterRecognition = OCR.OCR(source, "pol")
 # edges = krawedzie.krawedzie(source)
-scaled.show()
+filters = filtry.filtry(source, 5)
+# scaled.show()
 # grayed.show()
 # luminesence.show()
 # negative.show()
 # threshold.show()
 # print(opticalCharacterRecognition)
 # edges.show()
+filters.show()
 
-project.mask(source)
+# project.mask(source)
 
