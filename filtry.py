be49f0c5072ee2913ad2838d88d283d18f8e8835
Denis <rzyszka@tlen.pl>
2021-01-23 15:43:46 +0100

    GUI pre alpha

--- filtry.py
+++ filtry.py
@@ -8,30 +8,33 @@ def filtry (source, type):
     img = Image.open(source)
 
     if type == 1:
-        img1 = img.filter(CONTOUR)
+        img1 = img.filter(BLUR)
 
     if type == 2:
-        img1 = img.filter(DETAIL)
+        img1 = img.filter(CONTOUR)
 
     if type == 3:
-        img1 = img.filter(EDGE_ENHANCE)
+        img1 = img.filter(DETAIL)
 
     if type == 4:
-        img1 = img.filter(EDGE_ENHANCE_MORE)
+        img1 = img.filter(EDGE_ENHANCE)
 
     if type == 5:
-        img1 = img.filter(EMBOSS)
+        img1 = img.filter(EDGE_ENHANCE_MORE)
 
     if type == 6:
-        img1 = img.filter(FIND_EDGES)
+        img1 = img.filter(EMBOSS)
 
     if type == 7:
-        img1 = img.filter(SMOOTH)
+        img1 = img.filter(FIND_EDGES)
 
     if type == 8:
-        img1 = img.filter(SMOOTH_MORE)
+        img1 = img.filter(SMOOTH)
 
     if type == 9:
+        img1 = img.filter(SMOOTH_MORE)
+
+    if type == 10:
         img1 = img.filter(SHARPEN)
 
     return img1
